6f5110786f056ab6895ad2ee2384336cf5295990
pinpin <eva11980@icloud.com>
2022-07-19 18:48:17 +0800

    Initial Commit

---
+++ try storyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  try storyboard
+//
+//  Created by pinwei wang on 7/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
